36c2bc1117bbe2162b000ca331d3cd2694addf3d
Tiffany <tiffany_mollica@yahoo.com>
2020-07-12 12:11:22 -0400

    First Commit Portfolio Project

---
+++ js/testScripts.js
@@ -0,0 +1,38 @@
+$(function() {
+  //  $('.carousel').carousel({ interval: 1000 })
+   /* $("#carousel1").carousel( { interval: 5000 } );
+    $("#carouselButton1").click(function(){
+        if ($("#carouselButton1").children("i").hasClass("fa-pause")) {
+            $("#carousel1").carousel("pause");
+            $("#carouselButton1").children("i").removeClass("fa-pause");
+            $("#carouselButton1").children("i").addClass("fa-play");
+        } else {
+            $("#carousel1").carouse("cycle");
+            $("#carouselButton1").children("i").removeClass("fa-play");
+            $("#carouselButton1").children("i").addClass("fa-pause"); 
+        }
+    });*/
+
+   // $("#carousel2").carousel( { interval: 5000 } );
+    console.log($("#carouselButton2"));
+    b = $("#testButton");
+    console.log(b);
+    //b = document.getElementById("carouselButton2");
+    //b.addEventListener("click", function(){alert(1)});
+   /* $("#carouselButton2").click(
+        function(){
+            alert(1);
+        console.log($("#carouselButton2"));
+        if ($("#carouselButton2").children("i").hasClass("fa-pause")) {
+            console.log("click");
+            $("#carousel2").carousel("pause");
+            $("#carouselButton2").children("i").removeClass("fa-pause");
+            $("#carouselButton2").children("i").addClass("fa-play");
+        } else {
+            $("#carousel2").carousel("cycle");
+            $("#carouselButton2").children("i").removeClass("fa-play");
+            $("#carouselButton2").children("i").addClass("fa-pause"); 
+        }
+    }
+    );*/
+});
